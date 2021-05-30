0565898dbd3703a225e0dde8ad95b795a3c5e071
Stellshi <simplepro47@gmail.com>
2021-05-30 09:39:25 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Formacub
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i;
+            for (i = 1; i < 10; i++)
+                Console.WriteLine("{0}\t{1}\t{2}", i, i * i, i * i * i);
+        }
+    }
+}
